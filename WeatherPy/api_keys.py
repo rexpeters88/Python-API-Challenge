63629f4cffe1dd3798daeed3a6f289f5574473b3
Rex Peters <rexpeters@Rexs-MacBook-Pro.local>
2024-09-18 11:48:52 -0700

    Completed Notebook Scripts and Saved Output Images

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e536f6e7ef71234e74ab850bc77d823c"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0919b97992ef42f9840c555296d561f8"
